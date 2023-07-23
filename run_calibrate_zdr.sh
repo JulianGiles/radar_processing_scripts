cd7e4a2a6114eb0ca38227a5c2250cdc492a991b
Julian Alberto Giles <87534521+JulianGiles@users.noreply.github.com>
2023-07-23 21:19:37 +0200

    Create run_calibrate_zdr.sh

---
+++ run_calibrate_zdr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This file runs the calibrate_zdr.py script over several paths
+
+# Set the directory to look for the files
+dir=/automount/realpep/upload/jgiles/dwd/
+
+# Set the type of calibration method
+calibtype=1
+
+# Create a list of all files that include *allmoms* in their name
+files=$(find $dir -name "*allmoms*20150101*pro*" -type f)
+
+# Loop through each file in the list
+for file in $files; do
+
+  # Pass the file path to the python script
+  python /home/jgiles/Scripts/python/radar_processing_scripts/calibrate_zdr.py $file $calibtype
+
+done
