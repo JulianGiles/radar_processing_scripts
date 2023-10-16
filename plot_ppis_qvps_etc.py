fe64a18d1b2e6786b25803ff44653fa1a4a72d53
Julian Giles <julian23_g@hotmail.com>
2023-10-16 16:10:39 +0200

    Update plot_ppis_qvps_etc.py
    added full pipeline with phase processing, QVP, ML detection and entropy calculation

--- plot_ppis_qvps_etc.py
+++ plot_ppis_qvps_etc.py
@@ -48,30 +48,215 @@ except ModuleNotFoundError:
 import warnings
 warnings.filterwarnings('ignore')
 
-# we define a funtion to look for loc inside a path string
-def find_loc(locs, path):
-    components = path.split(os.path.sep)
-    for element in locs:
-        for component in components:
-            if element.lower() in component.lower():
-                return element
-    return None
 
-locs = ["pro", "tur", "umd", "afy", "ank", "gzt", "hty", "svs"]
+#%% Load and process data
 
-#%% Load data
-ff="/automount/realpep/upload/jgiles/dmi/2015/2015-09/2015-09-30/ANK/MON_YAZ_K/12.0/MON_YAZ_K-allmoms-12.0-20152015-092015-09-30-ANK-h5netcdf.nc"
-data=xr.open_dataset(ff)
+## Load the data into an xarray dataset (ds)
 
-# fix time dim in case some value is NaT
-if data.time.isnull().any():
-    data.coords["time"] = data["rtime"].min(dim="azimuth", skipna=True).compute()
+ff = "/automount/realpep/upload/jgiles/dwd/2017/2017-07/2017-07-25/pro/vol5minng01/07/*allmoms*"
+ds = utils.load_dwd_preprocessed(ff)
 
-# take time out of the coords if necessary
-for coord in ["latitude", "longitude", "altitude", "elevation"]:
-    if "time" in data[coord].dims:
-        data.coords[coord] = data.coords[coord].min("time")
+if "dwd" in ff:
+    country="dwd"
+    clowres0=True # this is for the ML detection algorithm
+elif "dmi" in ff:
+    country="dmi"
+    clowres0=False
 
+## Georeference 
+
+ds = ds.pipe(wrl.georef.georeference) 
+
+## Define minimum height
+
+min_height = utils.min_hgts["default"] + ds["altitude"].values
+
+## Get variable names
+
+X_DBZH, X_PHI, X_RHO, X_ZDR, X_TH = utils.get_names(ds)
+
+## Load ZDR offset
+
+# We define a custom exception to stop the next nexted loops as soon as a file is loaded
+class FileFound(Exception):
+    pass
+
+# Define the offset paths and file names or take them from the default
+
+zdroffdir = utils.zdroffdir
+zdrofffile = utils.zdrofffile
+
+# Load the offsets
+
+try:
+    for zdrod in zdroffdir:
+        for zdrof in zdrofffile:
+            try:
+                zdroffsetpath = os.path.dirname(utils.edit_str(ff, country, country+zdrod))
+                if "/VP/" in zdrod and "/vol5minng01/" in ff:
+                    elevnr = ff.split("/vol5minng01/")[-1][0:2]
+                    zdroffsetpath = utils.edit_str(zdroffsetpath, "/vol5minng01/"+elevnr, "/90gradstarng01/00")
+                    
+                ds = utils.load_ZDR_offset(ds, X_ZDR, zdroffsetpath+"/"+zdrof)
+                
+                # Change the default ZDR name to the corrected one
+                X_ZDR = X_ZDR+"_OC"
+                
+                # raise the custom exception to stop the loops
+                raise FileFound 
+                
+            except OSError:
+                pass
+            
+    # If no ZDR offset was loaded, print a message
+    print("No zdr offset to load: "+zdroffsetpath+"/"+zdrof)
+except FileFound:
+    pass
+
+
+## Load noise corrected RHOHV
+
+# Define the rhohv corrected paths and file names or take them from the default
+
+rhoncdir = utils.rhoncdir
+rhoncfile = utils.rhoncfile
+
+
+try:
+    rhoncpath = os.path.dirname(utils.edit_str(ff, country, country+rhoncdir))
+    
+    ds = utils.load_corrected_RHOHV(ds, rhoncpath+"/"+rhoncfile)
+    
+    # Check that the corrected RHOHV does not have much higher STD than the original (50% more)
+    # if that is the case we take it that the correction did not work well so we won't use it
+    if not (ds[X_RHO].std()*1.5 < ds["RHOHV_NC"].std()).compute():
+        # Change the default RHOHV name to the corrected one
+        X_RHO = X_RHO+"_NC"
+        
+except OSError:
+    print("No noise corrected rhohv to load: "+rhoncpath+"/"+rhoncfile)
+
+
+## Phase processing
+
+interpolation_method_ML = "linear" # for interpolating PHIDP in the ML
+
+
+phase_pross_params = {
+                        "dwd": {
+                            "window0": 7, # number of range bins for phidp smoothing (this one is quite important!)
+                            "winlen0": 7, # size of range window (bins) for the kdp-phidp calculations
+                            "xwin0": 9, # window size (bins) for the time rolling median smoothing in ML detection
+                            "ywin0": 1, # window size (bins) for the height rolling mean smoothing in ML detection
+                            "fix_range": 750, # range from where to consider phi values (dwd data is bad in the first bin)
+                        },
+                        "dmi": {
+                            "window0": 17,
+                            "winlen0": 21,
+                            "xwin0": 5,
+                            "ywin0": 5,
+                            "fix_range": 200,
+                        },
+}
+
+# Check that PHIDP is in data, otherwise skip ML detection
+if X_PHI in ds.data_vars:
+    # Set parameters according to data
+    
+    for param_name in phase_pross_params[country].keys():
+        globals()[param_name] = phase_pross_params[country][param_name]    
+    # window0, winlen0, xwin0, ywin0, fix_range = phase_pross_params[country].values() # explicit alternative
+
+    # phidp may be already preprocessed (turkish case), then proceed directly to masking and then vulpiani
+    if "UPHIDP" not in X_PHI:
+        # mask 
+        phi_masked = ds[X_PHI].where((ds[X_RHO] >= 0.95) & (ds[X_DBZH] >= 0.) & (ds["z"]>min_height) )
+        
+        # rename X_PHI as offset corrected
+        ds = ds.rename({X_PHI: X_PHI+"_OC"})
+
+    else:
+        ds = utils.phidp_processing(ds, X_PHI=X_PHI, X_RHO=X_RHO, X_DBZH=X_DBZH, rhohvmin=0.9,
+                             dbzhmin=0., min_height=0, window=window0, fix_range=fix_range)
+    
+        phi_masked = ds[X_PHI+"_OC_SMOOTH"].where((ds[X_RHO] >= 0.95) & (ds[X_DBZH] >= 0.) & (ds["z"]>min_height) )
+
+    # Assign phi_masked
+    assign = { X_PHI+"_OC_MASKED": phi_masked.assign_attrs(ds[X_PHI].attrs) }
+    ds = ds.assign(assign)
+    
+    # derive KDP from PHIDP (Vulpiani)
+
+    ds = utils.kdp_phidp_vulpiani(ds, winlen0, X_PHI+"_OC_MASKED", min_periods=winlen0/2)    
+    
+    X_PHI = X_PHI+"_OC" # continue using offset corrected PHI
+
+else:
+    print(X_PHI+" not found in the data, skipping ML detection")
+
+## Compute QVP
+
+ds_qvp = utils.compute_qvp(ds, min_thresh = {X_RHO:0.7, X_TH:0, X_ZDR:-1} )
+
+## Detect melting layer
+
+if X_PHI in ds.data_vars:
+    # Define thresholds
+    moments={X_DBZH: (10., 60.), X_RHO: (0.65, 1.), X_PHI: (-20, 180)}
+    
+    # Calculate ML
+    ds_qvp = utils.melting_layer_qvp_X_new(ds_qvp, moments=moments, 
+             dim="z", xwin=xwin0, ywin=ywin0, min_h=min_height, all_data=True, clowres=clowres0)
+    
+    # Assign ML values to dataset
+    
+    ds = ds.assign_coords({'height_ml': ds_qvp.height_ml})
+    ds = ds.assign_coords({'height_ml_bottom': ds_qvp.height_ml_bottom})
+
+    ds = ds.assign_coords({'height_ml_new_gia': ds_qvp.height_ml_new_gia})
+    ds = ds.assign_coords({'height_ml_bottom_new_gia': ds_qvp.height_ml_bottom_new_gia})
+
+## Attach ERA5 temperature profile
+loc = utils.find_loc(utils.locs, ff)
+ds_qvp = utils.attach_ERA5_TEMP(ds_qvp, path=loc.join(utils.era5_dir.split("loc")))
+ds = utils.attach_ERA5_TEMP(ds, path=loc.join(utils.era5_dir.split("loc")))
+
+## Discard possible erroneous ML values
+if "height_ml_new_gia" in ds_qvp:
+    isotherm = -1 # isotherm for the upper limit of possible ML values
+    z_isotherm = ds_qvp.TEMP.isel(z=((ds_qvp["TEMP"]-isotherm)**2).argmin("z").compute())["z"]
+    
+    ds_qvp.coords["height_ml_new_gia"] = ds_qvp["height_ml_new_gia"].where(ds_qvp["height_ml_new_gia"]<=z_isotherm.values).compute()
+    ds_qvp.coords["height_ml_bottom_new_gia"] = ds_qvp["height_ml_bottom_new_gia"].where(ds_qvp["height_ml_new_gia"]<=z_isotherm.values).compute()
+    
+    ds = ds.assign_coords({'height_ml_new_gia': ds_qvp.height_ml_new_gia})
+    ds = ds.assign_coords({'height_ml_bottom_new_gia': ds_qvp.height_ml_bottom_new_gia})
+
+## Fix KDP in the ML using PHIDP:
+if X_PHI in ds.data_vars:    
+    ds = utils.KDP_ML_correction(ds, X_PHI+"_MASKED", winlen=winlen0)
+
+    ds_qvp = ds_qvp.assign({"KDP_ML_corrected": utils.compute_qvp(ds)["KDP_ML_corrected"]})
+        
+## Classification of stratiform events based on entropy
+if X_PHI in ds.data_vars:    
+    
+    # calculate linear values for ZH and ZDR
+    ds = ds.assign({"DBZH_lin": wrl.trafo.idecibel(ds[X_DBZH]), "ZDR_lin": wrl.trafo.idecibel(ds[X_ZDR]) })
+    
+    # calculate entropy
+    Entropy = utils.Entropy_timesteps_over_azimuth_different_vars_schneller(ds, zhlin="DBZH_lin", zdrlin="ZDR_lin", rhohvnc=X_RHO, kdp="KDP_ML_corrected")
+    
+    # concate entropy for all variables and get the minimum value 
+    strati = xr.concat((Entropy.entropy_zdrlin, Entropy.entropy_Z, Entropy.entropy_RHOHV, Entropy.entropy_KDP),"entropy")        
+    min_trst_strati = strati.min("entropy")
+    
+    # assign to datasets
+    ds["min_entropy"] = min_trst_strati
+    
+    min_trst_strati_qvp = min_trst_strati.assign_coords({"z": ds["z"].median("azimuth")})
+    min_trst_strati_qvp = min_trst_strati_qvp.swap_dims({"range":"z"}) # swap range dimension for height
+    ds_qvp = ds_qvp.assign({"min_entropy": min_trst_strati_qvp})
 
 #%% Plot PPI
 
