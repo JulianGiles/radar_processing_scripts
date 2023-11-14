91a1ee07a04b0ec1504ca366c533d9b524bfa6a2
Julian Giles <julian23_g@hotmail.com>
2023-11-14 17:20:03 +0100

    Update run_calibrate_zdr.sh
    irrelevant

--- run_calibrate_zdr.sh
+++ run_calibrate_zdr.sh
@@ -2,13 +2,13 @@
 # This file runs the calibrate_zdr.py script over several paths
 
 # Set the directory to look for the files
-dir=/automount/realpep/upload/jgiles/dwd/
+dir="/automount/realpep/upload/jgiles/dwd/20*"
 
 # Set the type of calibration method
 calibtype=1
 
 # Create a list of all files that include *allmoms* in their name
-files=$(find $dir -name "*allmoms*20150101*pro*" -type f)
+files=$(find $dir -name "*90gradstarng01*allmoms*pro*" -type f)
 
 # Loop through each file in the list
 for file in $files; do
