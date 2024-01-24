6377b3760f28ecf9d6f4a21768baa9b3baeb4450
Julian Giles <julian23_g@hotmail.com>
2024-01-24 10:42:01 +0100

    Update run_calibrate_zdr.sh
    added checks for killing hanged script runs

--- run_calibrate_zdr.sh
+++ run_calibrate_zdr.sh
@@ -1,19 +1,70 @@
 #!/bin/bash
 # This file runs the calibrate_zdr.py script over several paths
 
-# Set the directory to look for the files
-dir="/automount/realpep/upload/jgiles/dwd/20*"
+# Se the directory to look for the files
+dir=/automount/realpep/upload/jgiles/dmi/
+
+# Which location to process
+loc=HTY
 
 # Set the type of calibration method
-calibtype=1
+calibtype=3
+
+max_attempts=5  # Maximum number of restart attempts
+max_execution_time=700  # Maximum execution time in seconds
 
 # Create a list of all files that include *allmoms* in their name
-files=$(find $dir -name "*90gradstarng01*allmoms*pro*" -type f)
+files=$(find $dir -name "*allmoms*$loc*" -type f -not -path "*qvp*"  -not -path "*WIND*" -not -path "*SURVEILLANCE*" -not -path "*RHI1*")
 
 # Loop through each file in the list
 for file in $files; do
+    attempt=1
+    while [ $attempt -le $max_attempts ]; do
 
-  # Pass the file path to the python script
-  python /home/jgiles/Scripts/python/radar_processing_scripts/calibrate_zdr.py $file $calibtype
+        # Invoke the Python script with the file path as an argument
+        python /home/jgiles/Scripts/python/radar_processing_scripts/calibrate_zdr.py $file $calibtype &
+
+        # Get the process ID of the background script
+        script_pid=$!
+
+        # set time counter and steps
+        timecount=0
+        sleepterval=5
+
+        while [ $timecount -le $max_execution_time ]; do
+            # sleep for a bit
+            sleep $sleepterval
+            timecount=$(( $timecount + $sleepterval ))
+
+            # Check if the script is still running
+            if ps -p $script_pid > /dev/null; then
+                : # do nothing
+            else
+                # Script finished successfully, break out of the loops
+                break 2
+            fi
+
+        done
+
+        # if after the max time the script is still running, kill it
+        kill $script_pid
+        echo "Script exceeded time limit. Killing and retrying..."
+        # clean the created folder
+        newfolder="${file/${dir}/${dir}rhohv_nc/}"
+        newfolder=$(dirname "$newfolder")
+        if [ -d "$newfolder" ]; then
+            rm -r "$newfolder" # clean the created folder
+        fi
+
+        ((attempt++))
+    done
+
+    if [ $attempt -gt $max_attempts ]; then
+        echo "Max restart attempts reached, could not be completed: $new_path"
+    fi
+
+
 
 done
+
+wait
