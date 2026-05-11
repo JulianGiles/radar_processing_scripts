22b57e7f06c26150531339f0eb3c3c5dea9970dc
Julian Giles <julian23_g@hotmail.com>
2026-05-11 16:47:31 +0200

    Updated slurm scripts to run computations in parallel

--- run_compute_qvps_parallel.sh
+++ run_compute_qvps_parallel.sh
@@ -8,30 +8,30 @@
 # Configurations for running in JUWELS:
 ##############################################################
 
-#SBATCH --account=detectrea
-#SBATCH --nodes=4
-#SBATCH --cpus-per-task=12
-#SBATCH --ntasks-per-node=4
-#SBATCH --time=01:00:00
-#SBATCH --job-name=compute_qvps_umd
-#SBATCH --output=compute_qvps_umd.out
-#SBATCH --error=compute_qvps_umd.err
+#SBATCH --account=detectrea2
+#SBATCH --nodes=8
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks-per-node=6
+#SBATCH --time=03:00:00
+#SBATCH --job-name=compute_qvps
+#SBATCH --output=compute_qvps.out
+#SBATCH --error=compute_qvps.err
 #SBATCH --open-mode=truncate
 #SBATCH --partition=batch
 export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
 
 # Set the directory with the code (this will be the working dir)
-codedir=/p/scratch/detectrea/giles1/radar_processing_scripts/
+codedir=/p/scratch/detectrea2/giles1/radar_processing_scripts/
 cd $codedir
 
 # Set the directory to look for the files
-dir=/p/scratch/detectrea/giles1/dwd/
+dir=/p/scratch/detectrea2/giles1/dwd/
 
 # set a name for the counter file (counting how many job steps running at the same time
-counterfile=$dir/count_umd.txt
+counterfile=$dir/count_qvps.txt
 
 # Create a list of all files that include *allmoms* in their name
-files=$(find $dir -name "*vol5minng01*allmoms_07*umd*" -type f -not -path "*qvp*"  -not -path "*ppi*" | sort -u)
+files=$(find $dir -name "*vol5minng01*allmoms_07*umd*" -type f -not -path "*qvp*"  -not -path "*ppi*" | sort -u) # "*vol5minng01*allmoms_07*umd*"
 
 count=0
 echo $count > $counterfile
@@ -39,18 +39,35 @@ startcount=0
 # Loop through each file in the list
 for file in $files; do
 
-    count=$(<$counterfile)
-    ((count++))
-    echo $count > $counterfile
-    ((startcount++))
-    # Pass the file path to the python script
-    { srun -c 12 --account=detectrea -n 1 --exact --threads-per-core=1 --time 10 python $codedir/compute_qvps_new.py $file; count=$(<$counterfile); ((count--)) ; echo $count > $counterfile; } &
+    # Get the last folder of the file path (indicating elevation) # REMEMBER DWD ELEVATION NUMBER IS NOT THE ACTUAL ELEVATION!
+    elev=$(basename "$(dirname "$file")")
 
-    if [ "$startcount" -le 30 ]; then
-        sleep 5
+    # Function to check if the number is between 7 and 15
+    is_between_0_and_15() {
+    local number_float=$1
+
+    # Use bc to convert the number string to float
+    local float_value=$(echo "scale=2; $number_float" | bc)
+
+    # Check if the float value is between 7 and 15 (inclusive)
+    (( $(bc <<< "$float_value >= 0.0 && $float_value <= 50.0") ))
+    }
+
+    # Check if the elevation is a valid float and within the desired range
+    if [[ $elev =~ ^[0-9]+(\.[0-9]+)?$ ]] && is_between_0_and_15 "$elev"; then
+        count=$(<$counterfile)
+        ((count++))
+        echo $count > $counterfile
+        ((startcount++))
+        # Pass the file path to the python script
+        { timeout 30m srun -c 8 --account=detectrea2 -n 1 --exact --threads-per-core=1 python -u $codedir/compute_qvps_new.py $file; count=$(<$counterfile); ((count--)) ; echo $count > $counterfile; } &
+
+        if [ "$startcount" -le 48 ]; then
+            sleep 5
+        fi
     fi
 
-    while [ "$(<$counterfile)" -ge 30 ]; do
+    while [ "$(<$counterfile)" -ge 48 ]; do
         sleep 5
     done
 
--- run_correct_rhohv_parallel.sh
+++ run_correct_rhohv_parallel.sh
@@ -8,25 +8,30 @@
 # Configurations for running in JUWELS:
 ##############################################################
 
-#SBATCH --account=detectrea
-#SBATCH --nodes=4
+#SBATCH --account=detectrea2
+#SBATCH --nodes=8
 #SBATCH --cpus-per-task=6
 #SBATCH --ntasks-per-node=8
-#SBATCH --time=24:00:00
-#SBATCH --output=correct_rhohv_pro.out
-#SBATCH --error=correct_rhohv_pro.err
-#SBATCH --open-mode=append
-#SBATCH --partition=mem192
+#SBATCH --time=06:00:00
+#SBATCH --output=correct_rhohv.out
+#SBATCH --error=correct_rhohv.err
+#SBATCH --open-mode=truncate
+#SBATCH --partition=batch
 export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
 
+# Set the directory with the code (this will be the working dir)
+codedir=/p/scratch/detectrea2/giles1/radar_processing_scripts/
+cd $codedir
+
 # Set the directory to look for the files
-dir=/p/scratch/detectrea/giles1/radar_data/dwd/
+dir=/p/scratch/detectrea2/giles1/dmi/
 
 # set a name for the counter file (counting how many job steps running at the same time
-counterfile=$dir/count_pro.txt
+counterfile=$dir/count_rhohv.txt
 
 # Create a list of all files that include *allmoms* in their name
-files=$(find $dir -name "*90gradstarng01*allmoms*pro*" -type f)
+files=$(find $dir -name "*allmom*" -type f -not -path "*qvp*"  -not -path "*WIND*" -not -path "*RHI1*" -not -path "*ppi*" | sort -u)
+
 
 count=0
 echo $count > $counterfile
@@ -39,7 +44,7 @@ for file in $files; do
     echo $count > $counterfile
     ((startcount++))
     # Pass the file path to the python script
-    { srun -c 6 --account=detectrea -n 1 --exact --threads-per-core=1  python $dir/correct_rhohv.py $file; count=$(<$counterfile); ((count--)) ; echo $count > $counterfile; } &
+    { srun -c 6 --account=detectrea2 -n 1 --exact --threads-per-core=1 --time 30  python -u $codedir/correct_rhohv.py $file; count=$(<$counterfile); ((count--)) ; echo $count > $counterfile; } &
 
     if [ "$startcount" -le 30 ]; then
         sleep 5
